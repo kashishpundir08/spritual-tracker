ad93bf1ec90cd468db4915ad81ac4855e40c816f
Kashish Pundir <pundirkashish972@gmail.com>
2026-05-16 12:20:09 +0530

    updated

--- backend/src/main/java/com/example/backend/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://vercel.com/kashish-pundirs-projects/spritual-tracker"  
+                "https://vercel.com/kashish-pundirs-projects/spritual-tracker"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
